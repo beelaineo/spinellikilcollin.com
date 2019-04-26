f2135aa59270d2286587ba8207658c19837c9f73
Joseph Thomas <joseph@good-idea.studio>
2019-04-26 15:17:37 -0700

    feat(use-shopify): add repo config

--- use-shopify/src/Provider/Provider.tsx
+++ use-shopify/src/Provider/Provider.tsx
@@ -1,27 +1,41 @@
 import * as React from 'react'
+import { Provider as UrqlProvider, createClient, Client } from 'urql'
 
 interface ShopifyContextValue {
-	/* */
+	client: Client
 }
 
-const initialValue = {
-	/* */
-}
-
-const ShopifyContext = React.createContext<ShopifyContextValue>(initialValue)
+const ShopifyContext = React.createContext<Partial<ShopifyContextValue>>({})
 
 export const ShopifyConsumer = ShopifyContext.Consumer
 
-export const useShopifyContext = () => React.useContext(ShopifyContext)
+export const useShopify = () => React.useContext(ShopifyContext)
 
 interface Props {
 	children: React.ReactNode
+	storefrontAccessToken: string
+	storefrontName: string
+	endpoint?: string
 }
 
-export const ShopifyProvider = ({ children }: Props) => {
+export const ShopifyProvider = ({ children, storefrontAccessToken, storefrontName, endpoint }: Props) => {
+	const url = endpoint || `https://${storefrontName}.myshopify.com/api/graphql`
+	const client = createClient({
+		url,
+		fetchOptions: {
+			headers: {
+				'X-Shopify-Storefront-Access-Token': storefrontAccessToken,
+			},
+		},
+	})
+
 	const value = {
-		/* */
+		client,
 	}
 
-	return <ShopifyContext.Provider value={value}>{children}</ShopifyContext.Provider>
+	return (
+		<UrqlProvider value={client}>
+			<ShopifyContext.Provider value={value}>{children}</ShopifyContext.Provider>
+		</UrqlProvider>
+	)
 }
