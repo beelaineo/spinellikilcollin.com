52fbaa77bbc492af3d2cf6059d03f5e2f4060ff7
Joseph Thomas <joseph@good-idea.studio>
2019-04-30 21:55:59 -0700

    fix(app): fix addToCart implementation

--- app/src/views/ProductDetail/components/BuyButton.tsx
+++ app/src/views/ProductDetail/components/BuyButton.tsx
@@ -7,7 +7,7 @@ interface Props extends Pick<UseCheckoutProps, 'addToCheckout'> {
 }
 
 export const BuyButton = ({ currentVariant, addToCheckout, quantity }: Props) => {
-	const handleClick = () => addToCheckout({ variantId: currentVariant.id, quantity: quantity || 1 })
+	const handleClick = () => addToCheckout({ lineItems: [{ variantId: currentVariant.id, quantity: quantity || 1 }] })
 	return (
 		<button disabled={Boolean(!currentVariant || currentVariant.availableForSale === false)} onClick={handleClick}>
 			buy it
