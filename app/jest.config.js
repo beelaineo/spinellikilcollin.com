120faaab0ef77e90afc5cdece39816058f2d6159
Joseph Thomas <joseph@good-idea.studio>
2019-05-10 14:53:37 -0700

    fix(app): fix tests, improve paths config

--- app/jest.config.js
+++ app/jest.config.js
@@ -8,4 +8,11 @@ module.exports = {
 	transform: {
 		'^.+\\.tsx?$': 'ts-jest',
 	},
+	moduleNameMapper: {
+		'test-utils/(.*)': '<rootDir>/src/test-utils/$1',
+		'Components/(.*)': '<rootDir>/src/components/$1',
+		'Views/(.*)': '<rootDir>/src/views/$1',
+		'Types/(.*)': '<rootDir>/src/types/$1',
+		'Providers/(.*)': '<rootDir>/src/providers/$1',
+	},
 }
--- app/src/views/ProductDetail/components/BuyButton.tsx
+++ app/src/views/ProductDetail/components/BuyButton.tsx
@@ -2,13 +2,13 @@ import * as React from 'react'
 import { Variant, UseCheckoutProps } from 'use-shopify'
 import { ButtonPrimary } from '../styled'
 
-interface Props extends Pick<UseCheckoutProps, 'addToCheckout'> {
+interface Props extends Pick<UseCheckoutProps, 'addItemToCheckout'> {
 	currentVariant?: Pick<Variant, 'id' | 'availableForSale'>
 	quantity?: number
 }
 
-export const BuyButton = ({ currentVariant, addToCheckout, quantity }: Props) => {
-	const handleClick = () => addToCheckout({ lineItems: [{ variantId: currentVariant.id, quantity: quantity || 1 }] })
+export const BuyButton = ({ currentVariant, addItemToCheckout, quantity }: Props) => {
+	const handleClick = () => addItemToCheckout({ variantId: currentVariant.id, quantity: quantity || 1 })
 	return (
 		<ButtonPrimary disabled={Boolean(!currentVariant || currentVariant.availableForSale === false)} onClick={handleClick}>
 			add to cart
