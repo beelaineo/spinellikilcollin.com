7bc7ce53cfc48351c0eff98689e3e5d040596c2f
Joseph Thomas <joseph@good-idea.studio>
2020-02-22 17:37:06 -0800

    fix(app): fix styled-components plugin

--- app/.babelrc.js
+++ app/.babelrc.js
@@ -1,3 +1,23 @@
+// module.exports = {
+//   presets: ['next/babel'],
+//   plugins: [['styled-components', { ssr: true, displayName: true }]],
+// }
 module.exports = {
   presets: ['next/babel'],
+  plugins: [
+    [
+      'babel-plugin-styled-components',
+      { ssr: true, displayName: true, preprocess: false },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: [
+        [
+          'babel-plugin-styled-components',
+          { ssr: true, displayName: false, preprocess: false },
+        ],
+      ],
+    },
+  },
 }
