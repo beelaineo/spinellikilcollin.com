3fcdf866590e70f0bca9b24a3327713b56c60345
Joseph Thomas <joseph@good-idea.studio>
2020-07-16 22:41:39 -0700

    fix(app): fall back to mp4 when m3u8 is not supported

--- app/src/components/CloudinaryVideo/CloudinaryVideo.tsx
+++ app/src/components/CloudinaryVideo/CloudinaryVideo.tsx
@@ -3,6 +3,7 @@ import Hls from 'hls.js'
 import { CloudinaryVideo as CloudinaryVideoType } from '../../types'
 import { AudioButton } from './AudioButton'
 import { VideoWrapper } from './styled'
+import { useViewportSize } from '../../utils'
 
 const { useRef, useEffect, useState } = React
 const BASE_URL = 'https://res.cloudinary.com/spinelli-kilcollin/video/upload'
@@ -17,13 +18,17 @@ const hlsConfig = {
   startLevel: 0,
 }
 
+const fallbackSizes = [720, 1200, 1600]
+
 export const CloudinaryVideo = ({ video }: CloudinaryVideoProps) => {
   if (!video?.videoId) return null
   const [ready, setReady] = useState(false)
+  const { width: viewportWidth } = useViewportSize()
+  const [fallbackSrc, setFallbackSrc] = useState<string | undefined>()
   const videoRef = useRef<HTMLVideoElement>(null)
   const hlsRef = useRef<Hls>()
   const [muted, setMuted] = useState(true)
-  const { enableAudio, enableControls, videoId } = video
+  const { enableAudio, videoId } = video
   const poster = `${BASE_URL}/c_scale,w_1200/${videoId}.jpeg`
 
   const toggleAudio = () => setMuted(!muted)
@@ -31,6 +36,7 @@ export const CloudinaryVideo = ({ video }: CloudinaryVideoProps) => {
   useEffect(() => {
     if (ready) return
     if (!videoRef.current) return
+    console.log(Hls.isSupported())
     if (Hls.isSupported()) {
       const hls = new Hls(hlsConfig)
       hlsRef.current = hls
@@ -46,12 +52,25 @@ export const CloudinaryVideo = ({ video }: CloudinaryVideoProps) => {
         videoRef.current.play()
       })
       setReady(true)
+    } else {
+      const bestSize =
+        fallbackSizes.find((fs) => fs > viewportWidth) ?? fallbackSizes[2]
+      const fallbackUrl = `https://res.cloudinary.com/spinelli-kilcollin/video/upload/c_scale,w_${bestSize}/${video.videoId}.mp4`
+      setFallbackSrc(fallbackUrl)
     }
   }, [ready, videoRef.current])
 
   return (
     <VideoWrapper>
-      <video autoPlay muted={muted} loop playsInline ref={videoRef} />
+      <video
+        poster={poster}
+        autoPlay
+        muted={muted}
+        loop
+        playsInline
+        ref={videoRef}
+        src={fallbackSrc}
+      />
       {enableAudio ? <AudioButton muted={muted} onClick={toggleAudio} /> : null}
     </VideoWrapper>
   )
