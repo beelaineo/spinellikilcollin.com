32bd843ede684e4d3311d52568a20780b9e1b80e
Joseph Thomas <joseph@good-idea.studio>
2020-08-05 13:59:13 -0700

    feat(app): install hubspot

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -118,19 +118,6 @@ const App = (props: AppProps) => {
                `,
             }}
           />
-          <script
-            /* Intercom */
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.intercomSettings = {
-                  app_id: "deljq5df"
-                };
-
-                // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/deljq5df'
-                (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/deljq5df';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-              `,
-            }}
-          />
         </Head>
         <Main>
           <Navigation />
@@ -148,6 +135,13 @@ const App = (props: AppProps) => {
           style={{ display: 'none', visibility: 'hidden' }}
         ></iframe>
       </noscript>
+      <script
+        type="text/javascript"
+        id="hs-script-loader"
+        async
+        defer
+        src="//js.hs-scripts.com/7668999.js"
+      />
     </Providers>
   )
 }
