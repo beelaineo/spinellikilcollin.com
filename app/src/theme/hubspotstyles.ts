87e6540510b700b9df9b8cb5fdb7a9e049522e22
Joseph Thomas <joseph@good-idea.studio>
2020-08-05 21:10:56 -0700

    fix(app): fix hubspot styles (2)

--- app/src/theme/hubspotstyles.ts
+++ app/src/theme/hubspotstyles.ts
@@ -11,8 +11,13 @@ export const hubspotStyles = css`
       font-family: serif;
       width: 200px;
       border: 0;
-
       box-shadow: 0 2px 6px 1px rgba(0, 0, 0, 0.2);
+      div#hs-eu-cookie-confirmation-inner a#hs-eu-confirmation-button,
+      div#hs-eu-cookie-confirmation-inner a#hs-eu-decline-button {
+        border-radius: 0;
+        text-transform: uppercase;
+        font-family: serif;
+      }
     }
     div#hs-eu-cookie-confirmation div#hs-eu-cookie-confirmation-inner {
       padding: 4;
@@ -26,6 +31,14 @@ export const hubspotStyles = css`
       div#hs-eu-cookie-confirmation-inner
       a#hs-eu-confirmation-button {
       margin-right: 0 !important;
+    }
+    @media only screen and (min-width: 960px) {
+      div#hs-eu-cookie-confirmation.hs-cookie-notification-position-bottom {
+        border-bottom: 0;
+        bottom: initial;
+        box-shadow: 0 2px 6px 1px rgba(0, 0, 0, 0.2);
+        top: 90px;
+      }
     }
   }
 `
