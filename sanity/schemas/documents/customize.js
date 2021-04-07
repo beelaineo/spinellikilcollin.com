d3552845817a0b6e9193e667f11be6a7d4b584e0
Joseph Thomas <joseph@good-idea.studio>
2021-04-07 10:40:13 -0700

    fix(repo): fix merge conflicts

--- sanity/schemas/documents/customize.js
+++ sanity/schemas/documents/customize.js
@@ -1,78 +1,62 @@
 export const customizeExamples = {
-  type: 'object',
-  name: 'customizeExamples',
-  title: 'Customize Examples',
+  type: "object",
+  name: "customizeExamples",
+  title: "Customize Examples",
   fields: [
     {
-      title: 'Title',
-      name: 'title',
-      type: 'string',
+      title: "Title",
+      name: "title",
+      type: "string",
     },
     {
-      title: 'Subtitle',
-      name: 'subtitle',
-      type: 'string',
+      title: "Subtitle",
+      name: "subtitle",
+      type: "string",
     },
     {
-      title: 'Links',
-      name: 'links',
-      type: 'array',
-      of: [{ type: 'imageTextBlock' }],
+      title: "Links",
+      name: "links",
+      type: "array",
+      of: [{ type: "imageTextBlock" }],
     },
   ],
-}
+};
 
-<<<<<<< HEAD
 export const experienceBlock = {
-  title: 'Experience Block',
-  name: 'experienceBlock',
-  type: 'object',
+  title: "Experience Block",
+  name: "experienceBlock",
+  type: "object",
   fields: [
     {
-      name: 'illustration',
-      title: 'Illustration',
-      type: 'richImage',
+      name: "illustration",
+      title: "Illustration",
+      type: "richImage",
     },
     {
-      name: 'heading',
-      title: 'Heading',
-      type: 'string',
+      name: "heading",
+      title: "Heading",
+      type: "string",
       validation: (Rule) => Rule.required(),
     },
     {
-      name: 'body',
-      title: 'Body',
-      type: 'text',
+      name: "body",
+      title: "Body",
+      type: "text",
     },
   ],
   preview: {
     select: {
-      title: 'heading',
-      subtitle: 'body',
-      media: 'illustration',
-=======
-export const customerStory = {
-  title: 'Customer Story',
-  name: 'customerStory',
-  type: 'object',
-  fields: [
-    { name: 'body', type: 'text', title: 'Quote' },
-    { name: 'byLine', type: 'string', title: 'Byline' },
-  ],
-  preview: {
-    select: {
-      title: 'byLine',
-      subtitle: 'body',
->>>>>>> main
+      title: "heading",
+      subtitle: "body",
+      media: "illustration",
     },
   },
-}
+};
 
-<<<<<<< HEAD
 export const experience = {
-  title: 'Experience',
-  type: 'object',
-  name: 'experience',
+  title: "Experience",
+  type: "object",
+  name: "experience",
   options: {
     collapsible: true,
     collapsed: true,
@@ -80,162 +64,138 @@ export const experience = {
 
   fields: [
     {
-      title: 'Title',
-      name: 'title',
-      type: 'string',
+      title: "Title",
+      name: "title",
+      type: "string",
     },
     {
-      title: 'Subtitle',
-      name: 'subtitle',
-      type: 'string',
+      title: "Subtitle",
+      name: "subtitle",
+      type: "string",
     },
     {
-      name: 'blocks',
-      title: 'Blocks',
-      type: 'array',
-      of: [{ type: 'experienceBlock' }],
+      name: "blocks",
+      title: "Blocks",
+      type: "array",
+      of: [{ type: "experienceBlock" }],
     },
   ],
-=======
-export const customerStories = {
-  title: 'Customer Stories',
-  name: 'customerStories',
-  type: 'object',
-  fields: [{ name: 'stories', type: 'array', of: [{ type: 'customerStory' }] }],
->>>>>>> main
-}
+};
 
 export const quizProductType = {
-  title: 'Quiz Product Type',
-  name: 'quizProductType',
-  type: 'object',
+  title: "Quiz Product Type",
+  name: "quizProductType",
+  type: "object",
   fields: [
-    { title: 'Title', name: 'title', type: 'string' },
+    { title: "Title", name: "title", type: "string" },
     {
-      title: 'Image',
-      name: 'image',
-      type: 'richImage',
+      title: "Image",
+      name: "image",
+      type: "richImage",
     },
   ],
-}
+};
 
 export const quizBlock = {
-  title: 'Quiz Block',
-  name: 'quizBlock',
-  type: 'object',
+  title: "Quiz Block",
+  name: "quizBlock",
+  type: "object",
   fields: [
     {
-      title: 'Title',
-      name: 'title',
-      type: 'string',
+      title: "Title",
+      name: "title",
+      type: "string",
     },
     {
-      title: 'Subtitle',
-      name: 'subtitle',
-      type: 'string',
+      title: "Subtitle",
+      name: "subtitle",
+      type: "string",
     },
     {
-      title: 'Image',
-      name: 'image',
-      type: 'richImage',
+      title: "Image",
+      name: "image",
+      type: "richImage",
     },
   ],
-}
+};
 
 export const customize = {
-  type: 'document',
-  name: 'customize',
-  title: 'Customize',
+  type: "document",
+  name: "customize",
+  title: "Customize",
   fieldsets: [
-<<<<<<< HEAD
     {
-      name: 'quiz',
-      title: 'Quiz',
+      name: "quiz",
+      title: "Quiz",
       options: {
         collapsible: true,
         collapsed: true,
       },
     },
     {
-      name: 'examples',
-      title: 'Examples',
+      name: "examples",
+      title: "Examples",
       options: {
         collapsible: true,
         collapsed: true,
       },
     },
-=======
-    { name: 'quiz', title: 'Quiz' },
-    { name: 'stories', title: 'Customer Stories' },
-    { name: 'examples', title: 'Examples' },
->>>>>>> main
   ],
   fields: [
     {
-      title: 'Title',
-      name: 'title',
-      type: 'string',
+      title: "Title",
+      name: "title",
+      type: "string",
     },
     {
-      name: 'hero',
-      type: 'hero',
+      name: "hero",
+      type: "hero",
     },
     {
-      name: 'subtitle',
-      label: 'Subtitle',
-      type: 'string',
+      name: "subtitle",
+      label: "Subtitle",
+      type: "string",
     },
     {
-      name: 'body',
-      label: 'Content',
-      type: 'richText',
+      name: "body",
+      label: "Content",
+      type: "richText",
     },
     {
-      name: 'quizBlock',
-      title: 'Quiz Block',
-      fieldset: 'quiz',
-      type: 'quizBlock',
+      name: "quizBlock",
+      title: "Quiz Block",
+      fieldset: "quiz",
+      type: "quizBlock",
     },
     {
-      name: 'quizProductTypes',
-      title: 'Product Types',
-      fieldset: 'quiz',
-      type: 'array',
-      of: [{ type: 'quizProductType' }],
+      name: "quizProductTypes",
+      title: "Product Types",
+      fieldset: "quiz",
+      type: "array",
+      of: [{ type: "quizProductType" }],
     },
     {
-      name: 'quizStyles',
-      title: 'Quiz Styles',
-      fieldset: 'quiz',
-      type: 'array',
-      of: [{ type: 'string' }],
+      name: "quizStyles",
+      title: "Quiz Styles",
+      fieldset: "quiz",
+      type: "array",
+      of: [{ type: "string" }],
     },
     {
-<<<<<<< HEAD
-      name: 'experience',
-      title: 'Experience',
-      type: 'experience',
-=======
-      name: 'customerStories',
-      title: 'Customer Stories',
-      fieldset: 'stories',
-      type: 'customerStories',
->>>>>>> main
+      name: "experience",
+      title: "Experience",
+      type: "experience",
     },
     {
-      name: 'examples',
-      title: 'Examples',
-      fieldset: 'examples',
-      type: 'customizeExamples',
+      name: "examples",
+      title: "Examples",
+      fieldset: "examples",
+      type: "customizeExamples",
     },
-<<<<<<< HEAD
-=======
-
->>>>>>> main
     {
-      name: 'seo',
-      title: 'SEO',
-      type: 'seo',
+      name: "seo",
+      title: "SEO",
+      type: "seo",
     },
   ],
-}
+};
