a03a6ccf0fecb747d443eff31e151d67184a82d9
Joseph Thomas <joseph@good-idea.studio>
2021-04-13 21:08:36 -0700

    feat(app): set up next.js redirects from CSV"

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
+import { DocumentContext } from 'next'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import { gtm } from './_app'
 import { config } from '../src/config'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
