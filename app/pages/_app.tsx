932bd315603927e505e82e12d6967af9a9b5b7ad
Hon Chih Chen <honchih.chen@roswellstudios.com>
2021-05-17 14:27:50 -0400

    feat(app): add Facebook Pixel code

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -67,7 +67,7 @@ export const gtm = {
   dev: { script: '', iframeSrc: '' },
 }
 
-const { STOREFRONT_ENV } = config
+const { STOREFRONT_ENV, FB_PIXEL_ID } = config
 
 const App = (props: AppProps) => {
   const { Component, pageProps: allPageProps, router } = props
@@ -146,6 +146,33 @@ const App = (props: AppProps) => {
                `,
             }}
           />
+          {/* Global Site Code Pixel - Facebook Pixel */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                if (window && typeof(fbq) === 'undefined') {
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', ${FB_PIXEL_ID});
+                  fbq('track', 'PageView');
+                }
+              `,
+            }}
+          />
+          {/* <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript> */}
         </Head>
         <Main>
           <Navigation />
