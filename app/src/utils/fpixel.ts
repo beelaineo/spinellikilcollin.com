1e45c3d2547158ef5a25f40568f4444d1dd08a70
Hon Chih Chen <honchih.chen@roswellstudios.com>
2021-05-17 14:28:52 -0400

    feat(app): facebook pixel events

---
+++ app/src/utils/fpixel.ts
@@ -0,0 +1,17 @@
+
+export const pageview = () => {
+  window.fbq('track', 'PageView')
+}
+
+// https://developers.facebook.com/docs/facebook-pixel/advanced/
+export const event = (name, options = {}) => {
+  window.fbq('track', name, options)
+}
+
+export const viewContent = (options = {}) => {
+  event('ViewContent', options)
+}
+
+export const addToCart = (options = {}) => {
+  event('AddToCart')
+}
