57da3805fd305e2331aa84f500b58ac719122378
Blaine <blaine.d.oneill@gmail.com>
2021-09-30 14:50:41 -0700

    fix(app): add reportFBAddToCart event

--- app/src/utils/fpixel.ts
+++ app/src/utils/fpixel.ts
@@ -32,6 +32,58 @@ type FB_VIEW_CONTENT = {
   currency?: string
 }
 
+export const reportFBAddToCart = (product: ShopifyProduct): void => {
+  const content_ids: string[] = []
+  const event_id =
+    'ATC_' + new Date().getTime() + Math.floor(100000 + Math.random() * 900000)
+  const eventData = { eventID: event_id }
+  let contentName
+  let template: FB_VIEW_CONTENT
+  const hash = getLocationSearchHash(window.location.search)
+  const productId = getIdFromBase64(hash)
+  if (productId) {
+    content_ids.push(productId)
+
+    template = {
+      content_type: 'product',
+      product_catalog_id: `${FB_PRDOUCT_CATALOG_ID}`,
+      content_ids: content_ids,
+    }
+
+    const variants = product.variants
+    const variant = variants?.find((v) => {
+      return v?.shopifyVariantID === hash
+    })
+
+    if (variant && variant.sourceData) {
+      contentName = `${product.title}`
+      if (
+        variant.sourceData.selectedOptions &&
+        variant.sourceData.selectedOptions.length
+      ) {
+        variant.sourceData.selectedOptions.forEach((o) => {
+          contentName = `${contentName} - ${o?.name}:${o?.value}`
+        })
+        template['content_name'] = contentName
+      }
+
+      if (variant.sourceData.priceV2) {
+        if (variant.sourceData.priceV2.amount) {
+          template['value'] = parseFloat(variant.sourceData.priceV2.amount)
+        }
+        if (variant.sourceData.priceV2.currencyCode) {
+          template['currency'] = variant.sourceData.priceV2.currencyCode
+        }
+      }
+
+      if (product.sourceData && product.sourceData.productType) {
+        template['content_category'] = product.sourceData.productType
+      }
+      addToCart(template, eventData)
+    }
+  }
+}
+
 export const reportFBViewContent = (product: ShopifyProduct): void => {
   const content_ids: string[] = []
   const event_id =
@@ -79,7 +131,6 @@ export const reportFBViewContent = (product: ShopifyProduct): void => {
       if (product.sourceData && product.sourceData.productType) {
         template['content_category'] = product.sourceData.productType
       }
-      console.log(template, eventData)
       viewContent(template, eventData)
     }
   }
