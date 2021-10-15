78702230069840660c330d540cecaf30ce6c2b95
Blaine <blaine.d.oneill@gmail.com>
2021-10-15 02:16:06 -0700

    feat(sanity): add 'Open in Preview' action to sanity documents, resolveProductionUrl

--- sanity/resolveProductionUrl.js
+++ sanity/resolveProductionUrl.js
@@ -1,3 +1,5 @@
 export default function resolveProductionUrl(document) {
-  return `https://spinellikilcollin.com/${document.slug.current}`
+  const readToken =
+    'sk56XjAXI9dV5P1yFoPyGIOB3c9Gb52XjGDB7YBrwO41mVRc3c4wq4Uvu8nNMWgRWxUmvHGLBozFm0U2QGcqfbYu8XrCmbzyv64wcwYOukq6fjhepAdrvgMNVtLdcVaBzapYOGMyZmnOxEtNGiMs8iQijid9aR7MMv1XYYHaqTBrfEmiVmWQ'
+  return `https://spinellikilcollin.com/products/${document.handle}?preview=${readToken}`
 }
