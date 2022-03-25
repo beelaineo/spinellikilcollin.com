42503a223a27fd63949a825c6fbe8864b55a2d32
Joseph <joseph@good-idea.studio>
2022-03-25 15:15:51 -0700

    chore(repo): update some dependencies, add import/no-cycle to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: [
     'prettier',
     'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -10,17 +12,18 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 1,
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
+    'import/no-cycle': 'error',
+    // 'react/prop-types': 'off',
+    // 'no-unused-vars': 'off',
+    // '@typescript-eslint/no-unused-vars': 1,
+    // // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // '@typescript-eslint/no-inferrable-types': 'off',
+    // '@typescript-eslint/explicit-member-accessibility': 'off',
+    // '@typescript-eslint/ban-ts-ignore': 'off',
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/camelcase': 'off',
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/ban-ts-comment': 'off',
   },
   settings: {
     react: {
