33b4cffe082a026389d0d318eb8f1c72b404904c
Blaine <blaine.d.oneill@gmail.com>
2022-05-25 20:42:30 -0700

    fix(app): remove newsletter signup id attribute (element ids must be unique)

--- app/src/components/Footer/NewsletterSignup.tsx
+++ app/src/components/Footer/NewsletterSignup.tsx
@@ -48,7 +48,7 @@ export const NewsletterSignup = ({
       <Heading mb={3} level={5}>
         {mailerSubtitle}
       </Heading>
-      <MailerForm id="newsletter-signup" onSubmit={handleSubmit}>
+      <MailerForm onSubmit={handleSubmit}>
         <SuccessWrapper visible={success}>
           <Heading my={0} color="body.6" textAlign="left" level={5}>
             Thank you! You have been subscribed.
