0e92ee387af5990844e1794a352e3c2e8571784b
Blaine <blaine.d.oneill@gmail.com>
2022-06-08 13:36:26 +0200

    fix(app): add id attributes to ProductAccordions

--- app/src/components/Accordion/Accordion.tsx
+++ app/src/components/Accordion/Accordion.tsx
@@ -20,8 +20,19 @@ export const Accordion = ({ label, children }: AccordionProps) => {
     updateHeight(refContainer.current.clientHeight)
   }, [refContainer])
 
+  const formatSelector = (label: string) => {
+    return label
+      .concat(' accordion')
+      .normalize('NFKD') // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+      .toLowerCase() // Convert the string to lowercase letters
+      .trim() // Remove whitespace from both sides of a string (optional)
+      .replace(/\s+/g, '-') // Replace spaces with -
+      .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+      .replace(/\-\-+/g, '-') // Replace multiple - with single -
+  }
+
   return (
-    <Wrapper>
+    <Wrapper id={formatSelector(label)}>
       <Label onClick={toggleOpen}>
         {label}
         <PlusMinus open={open} />
