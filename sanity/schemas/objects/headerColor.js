41d1fd9117bfbfc259bf747be05434da769cf2fc
julian stein <julian.stein@gmail.com>
2022-10-09 16:49:47 -0700

    feat(sanity): add headerColor object to schema

---
+++ sanity/schemas/objects/headerColor.js
@@ -0,0 +1,13 @@
+export const headerColor = {
+  title: 'Header/Nav Color',
+  name: 'header_color',
+  description: 'Text color for the header/nav overlay over the hero.',
+  type: 'string',
+  options: {
+    list: [
+      { title: 'Dark (Default)', value: 'dark' },
+      { title: 'Light', value: 'light' },
+    ],
+    layout: 'radio',
+  },
+}
