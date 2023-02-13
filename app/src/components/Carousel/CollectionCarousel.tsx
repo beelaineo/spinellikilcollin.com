bdb662360489d4997fcaf2a9644b646482205344
julian stein <julian.stein@gmail.com>
2023-02-12 23:13:43 -0800

    feat(app): move carousel fetch to page query

--- app/src/components/Carousel/CollectionCarousel.tsx
+++ app/src/components/Carousel/CollectionCarousel.tsx
@@ -67,28 +67,11 @@ interface CollectionCarouselProps {
 }
 
 export const CollectionCarousel = ({ collection }: CollectionCarouselProps) => {
-  const collectionProducts = collection?.products
+  const products = collection?.products
   const { width: viewportWidth } = useViewportSize()
-  const variables = {
-    collectionId: collection._id,
-  }
-  const [getCarousel, response] = useLazyRequest<Response, Variables>(
-    query,
-    variables,
-  )
-  const { data } = response
 
-  useEffect(() => {
-    if (Boolean(data)) return
-    if (!variables.collectionId) return
-    if (collectionProducts && collectionProducts.length) return
-    getCarousel(variables)
-  }, [data])
+  if (!products.length) return
 
-  const fetchedCollection = data?.allShopifyCollection[0]
-  const products = definitely(fetchedCollection?.products)
-
-  if (!products.length) return null
 
   const initialSlide = viewportWidth < 650 ? 1 : 0
   return (
