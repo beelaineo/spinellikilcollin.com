7b7d8bec3635d48381725d771c00b266fe3394f8
julian stein <julian.stein@gmail.com>
2023-02-20 09:05:57 -0800

    fix(app): add query back to CollectionCarousel

--- app/src/components/Carousel/CollectionCarousel.tsx
+++ app/src/components/Carousel/CollectionCarousel.tsx
@@ -1,16 +1,91 @@
 import * as React from 'react'
+import { gql } from 'graphql-tag'
 import { ShopifyCollection } from '../../types'
 import { Carousel } from './Carousel'
 import { ProductThumbnail } from '../Product'
 import { definitely, useViewportSize } from '../../utils'
+import { useLazyRequest, shopifySourceImageFragment } from '../../graphql'
+const { useEffect } = React
+
+const query = gql`
+  query CarouselCollectionQuery($collectionId: ID!) {
+    allShopifyCollection(where: { _id: { eq: $collectionId } }) {
+      __typename
+      _id
+      _type
+      _key
+      title
+      handle
+      archived
+      shopifyId
+      products {
+        __typename
+        _id
+        _key
+        title
+        hidden
+        hideFromSearch
+        handle
+        archived
+        shopifyId
+        minVariantPrice
+        maxVariantPrice
+        sourceData {
+          __typename
+          id
+          title
+          handle
+          tags
+          productType
+          images {
+            __typename
+            edges {
+              __typename
+              cursor
+              node {
+                ...ShopifySourceImageFragment
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  ${shopifySourceImageFragment}
+`
+
+interface Response {
+  allShopifyCollection: ShopifyCollection[]
+}
+interface Variables {
+  collectionId: string | null | undefined
+}
 
 interface CollectionCarouselProps {
   collection: ShopifyCollection
 }
 
 export const CollectionCarousel = ({ collection }: CollectionCarouselProps) => {
-  const products = collection?.products
+  const collectionProducts = collection?.products
   const { width: viewportWidth } = useViewportSize()
+
+  const variables = {
+    collectionId: collection._id,
+  }
+  const [getCarousel, response] = useLazyRequest<Response, Variables>(
+    query,
+    variables,
+  )
+  const { data } = response
+
+  useEffect(() => {
+    if (Boolean(data)) return
+    if (!variables.collectionId) return
+    if (collectionProducts && collectionProducts.length) return
+    getCarousel(variables)
+  }, [data])
+
+  const fetchedCollection = data?.allShopifyCollection[0]
 
   if (!products?.length) return null
 
