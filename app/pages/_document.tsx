a2027a8a30acf7a7343ba4b70267733bb3f5a3a3
Blaine <blaine.d.oneill@gmail.com>
2023-03-30 09:46:37 -0700

    feat(app): add aspire tracking script

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -98,6 +98,19 @@ export default class MyDocument extends Document {
             }}
             strategy="beforeInteractive"
           />
+          {/* Aspire */}
+          <Script
+            id="aspire-script"
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(){var o=window.tdl=window.tdl||[];if(o.invoked)window.console&&console.error&&console.error("Tune snippet has been included more than once.");else{o.invoked=!0,o.methods=["init","identify","convert"],o.factory=function(n){return function(){var e=Array.prototype.slice.call(arguments);return e.unshift(n),o.push(e),o}};for(var e=0;e<o.methods.length;e++){var n=o.methods[e];o[n]=o.factory(n)}o.init=function(e){var n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://js.go2sdk.com/v2/tune.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(n,t),o.domain=e}}}();
+
+              tdl.init("https://aspireiq.go2cloud.org") 
+              tdl.identify() 
+            `,
+            }}
+            strategy="beforeInteractive"
+          />
           {/* Global Site Code Pixel - TikTok */}
           <Script
             id="tik-tok-pixel"
