44314cbfe01cd365aa7a14261a2f8189c8502dd9
julian stein <julian.stein@gmail.com>
2024-03-27 17:12:38 -0700

    feat(app): adjust spacing

--- app/src/components/CountDown/styled.tsx
+++ app/src/components/CountDown/styled.tsx
@@ -1,30 +1,116 @@
 import styled, { css, DefaultTheme } from '@xstyled/styled-components'
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 8;
-  position: relative;
-  justify-content: center;
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: row;
+    gap: 6;
+    position: relative;
+    justify-content: center;
 
-  > div:last-child{
-    div::after {
-        content: '';
-  }
+    ${theme.mediaQueries.tablet} {
+      gap: 4;
+    }
+    ${theme.mediaQueries.mobile} {
+      gap: 4;
+    }
+  `}
 `
 
 export const Column = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 50px;
+
+    &::after {
+      content: ':';
+      display: block;
+      position: absolute;
+      bottom: 3px;
+      right: -19px;
+      color: white;
+      z-index: 1;
+      font-size: ${theme.fontSizes[4]};
+      margin: 0;
+      color: inherit;
+    }
+    &:last-child {
+      &::after {
+        display: none;
+      }
+    }
+
+    &:first-child {
+      &::after {
+        right: -15px;
+      }
+    }
+
+    &:nth-child(2) {
+      &::after {
+        right: -23px;
+      }
+    }
+
+    &:nth-child(3) {
+      &::after {
+        right: -20px;
+      }
+    }
+
+    ${theme.mediaQueries.tablet} {
+      width: 34px;
+
+      &:first-child {
+        &::after {
+          right: -8px;
+        }
+      }
+
+      &:nth-child(2) {
+        &::after {
+          right: -13px;
+        }
+      }
+
+      &:nth-child(3) {
+        &::after {
+          right: -10px;
+        }
+      }
+
+      &::after {
+        font-size: ${theme.fontSizes[5]};
+      }
+    }
+    ${theme.mediaQueries.mobile} {
+      &::after {
+        font-size: ${theme.fontSizes[5]};
+      }
+    }
+  `}
 `
 
 export const Value = styled.div`
   ${({ theme }) => css`
     span {
       font-size: ${theme.fontSizes[2]};
+    }
+
+    ${theme.mediaQueries.tablet} {
+      span {
+        font-size: ${theme.fontSizes[3]};
+      }
+    }
+
+    ${theme.mediaQueries.mobile} {
+      span {
+        font-size: ${theme.fontSizes[3]};
+      }
     }
   `}
 `
@@ -40,17 +126,20 @@ export const Label = styled.div`
     gap: 4;
 
     span {
-      font-size: ${theme.fontSizes[3]};
+      font-size: ${theme.fontSizes[4]};
+      margin: 0;
     }
-
-    &::after {
-      content: ':';
-      display: block;
-      position: absolute;
-
-      right: -24px;
-      color: white;
-      z-index: 1;
+    ${theme.mediaQueries.tablet} {
+      span {
+        font-size: ${theme.fontSizes[5]};
+      }
     }
+    ${theme.mediaQueries.mobile} {
+      span {
+        font-size: ${theme.fontSizes[5]};
+      }
+    }
+    last-of-type {
+      gap: 0;
   `}
 `
