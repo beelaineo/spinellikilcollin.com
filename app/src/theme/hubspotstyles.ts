1bb5eb10ff661744a600ff5bbd33214812496c1d
Blaine <blaine.d.oneill@gmail.com>
2024-07-23 18:48:32 -0700

    fix(app): increase z-index of hubspot iframe container

--- app/src/theme/hubspotstyles.ts
+++ app/src/theme/hubspotstyles.ts
@@ -4,7 +4,7 @@ export const hubspotStyles = css`
   ${({ theme }) => css`
     body {
       div#hubspot-messages-iframe-container {
-        z-index: 12;
+        z-index: 101;
       }
       div#hs-eu-cookie-confirmation {
         position: fixed;
